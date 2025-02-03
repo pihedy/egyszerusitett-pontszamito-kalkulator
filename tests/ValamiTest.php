004313da33ea0373e7eccb1077113a336cbce64c
pihedy <pihedy@gmail.com>
2025-02-03 19:28:00 +0100

    Init project files.

---
+++ tests/ValamiTest.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+use \PHPUnit\Framework\TestCase;
+
+class ValamiTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
