76c95256f91f1bb79a6ddc842f64fe4e8a998f9c
pihedy <pihedy@gmail.com>
2025-02-03 20:02:19 +0100

    Basic runtime run file basic logic.

--- run.php
+++ run.php
@@ -1,3 +1,28 @@
 <?php declare(strict_types=1);
 
-$valami = 'asdasdasd';
+/**
+ * @author Pihe Edmond <pihedy@gmail.com>
+ */
+
+try {
+    /**
+     * Autoload files using Composer autoload
+     */
+    require_once __DIR__ . '/src/vendor/autoload.php';
+
+    if (!file_exists(__DIR__ . '/data/homework_input.php')) {
+        throw new \Exception('Please create homework_input.php file in data folder');
+    }
+
+    /**
+     * Include homework_input.php file.
+     */
+    include __DIR__ . '/data/homework_input.php';
+
+    /**
+     * @var array Required configuration data.
+     */
+    $requirements = include __DIR__ . '/config/requirements.php' ?? [];
+} catch (\Exception $Exception) {
+    echo $Exception->getMessage();
+}
