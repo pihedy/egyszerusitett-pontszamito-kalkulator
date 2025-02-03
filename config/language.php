032911e1a5140c8fb9fa195220496095b0ea3490
pihedy <pihedy@gmail.com>
2025-02-03 19:52:20 +0100

    Including a language file, which we'll see if we need. For now, it contains the Hungarian equivalent of the keys.

---
+++ config/language.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+/**
+ * @author Pihe Edmond <pihedy@gmail.com>
+ */
+
+return [
+    'computer_science' => 'Programtervező informatikus',
+    'anglistics' => 'Anglisztika',
+    'mathematics' => 'Matematika',
+    'biology' => 'Biológia',
+    'physics' => 'Fizika',
+    'informatics' => 'Informatika',
+    'chemistry' => 'Kémia',
+    'english' => 'Angol',
+    'french' => 'Francia',
+    'german' => 'Német',
+    'italian' => 'Olasz',
+    'russian' => 'Orosz',
+    'spanish' => 'Spanyol',
+    'history' => 'Történelem',
+    'hungarian_language_and_literature' => 'Magyar nyelv és irodalom',
+    'b2' => 'B2 Középfokú nyelvvizsga',
+    'c1' => 'C1 Felsőfokú nyelvvizsga',
+    'advanced_level_points' => 'Emelt szintű érettségi pontok',
+    'max_bonus_points' => 'Maximális többletpont',
+    'failed_threshold' => 'Sikertelen érettségi küszöb'
+];
